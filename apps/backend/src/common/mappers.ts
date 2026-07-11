bbeba0f7f0c52cb72d6021f0d2a2157110fd3cd9
Callcold1 <kevin.mooney@hotmail.com>
2026-07-11 19:29:18 +0200

    Added hashing to changePassword function

--- apps/backend/src/common/mappers.ts
+++ apps/backend/src/common/mappers.ts
@@ -33,7 +33,6 @@ export function toApiUser(user: UserWithMemberships): User {
   return {
     id: user.id,
     name: user.name,
-    // password: user.password,
     email: user.email,
     isMemberOf,
     isAdminOf,
